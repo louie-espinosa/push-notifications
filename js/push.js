bcc122b5b41ae2760906485831b30d2b1549fb16
Louie Espinosa <louie.espinosa@proton.me>
2023-12-09 13:24:15 -0600

    Removed alert message and replaced with conditional before creating new instance of Notification

--- js/push.js
+++ js/push.js
@@ -2,9 +2,13 @@
 
 console.log("Hello, World!");
 
+//need conditional to run if permission is granted
+
 const button = document.querySelector('button')
   button.addEventListener("click", function() {
   Notification.requestPermission().then(permission => {
-    alert(permission)
+    if(permission === 'granted') {
+      new Notification("Example ")
+    }
   })
 })
