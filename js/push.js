c356f9ec9913a44da86d530a5477bdc7f5a015e9
Louie Espinosa <louie.espinosa@proton.me>
2023-12-09 18:04:22 -0600

    created body proerty and data attribute for notification; created event listener (close event) to log message when user closes notification

--- js/push.js
+++ js/push.js
@@ -8,7 +8,21 @@ const button = document.querySelector('button')
   button.addEventListener("click", function() {
   Notification.requestPermission().then(permission => {
     if(permission === 'granted') {
-      new Notification("Example ")
+      new Notification("Example Notification", {
+        body: "This text is written on the body property",
+        data: {hello: "World"}
+      })
+      alert(Notification.data.hello)
     }
   })
 })
+//created variable, notification, and added event listener (close event) to log the message upon closing notification
+const notification = new Notification("example notification", {
+  body: "this is more text",
+  data: {hello: "world"}
+})
+notification.addEventListener("close", e => {
+  console.log(e.target.data.hello);
+})
+//need to dl icons and save in icon dir INSIDE current dir
+//I can use these with icon property 
